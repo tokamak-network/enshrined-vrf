68b3152268dcde9b7779e016ef259f35d3f45c1f
Ishika Choudhury <117741714+Rimeeeeee@users.noreply.github.com>
2025-05-03 13:00:52 +0530

    refactor: added const without seperator for base mainnet (alloy-rs/hardforks#37)

--- src/base/mainnet.rs
+++ src/base/mainnet.rs
@@ -2,19 +2,19 @@
 
 use crate::optimism::mainnet::*;
 
-/// Bedrock base hardfork activation block
+/// Bedrock base hardfork activation block is 0.
 pub const BASE_MAINNET_BEDROCK_BLOCK: u64 = 0;
-/// Regolith base hardfork activation timestamp
+/// Regolith base hardfork activation timestamp is 0.
 pub const BASE_MAINNET_REGOLITH_TIMESTAMP: u64 = OP_MAINNET_REGOLITH_TIMESTAMP;
-/// Canyon base hardfork activation timestamp
+/// Canyon base hardfork activation timestamp is 1704992401.
 pub const BASE_MAINNET_CANYON_TIMESTAMP: u64 = OP_MAINNET_CANYON_TIMESTAMP;
-/// Ecotone base hardfork activation timestamp
+/// Ecotone base hardfork activation timestamp is 1710374401.
 pub const BASE_MAINNET_ECOTONE_TIMESTAMP: u64 = OP_MAINNET_ECOTONE_TIMESTAMP;
-/// Fjord base hardfork activation timestamp
+/// Fjord base hardfork activation timestamp is 1720627201.
 pub const BASE_MAINNET_FJORD_TIMESTAMP: u64 = OP_MAINNET_FJORD_TIMESTAMP;
-/// Granite base hardfork activation timestamp
+/// Granite base hardfork activation timestamp is 1726070401.
 pub const BASE_MAINNET_GRANITE_TIMESTAMP: u64 = OP_MAINNET_GRANITE_TIMESTAMP;
-/// Holocene base hardfork activation timestamp
+/// Holocene base hardfork activation timestamp is 1736445601.
 pub const BASE_MAINNET_HOLOCENE_TIMESTAMP: u64 = OP_MAINNET_HOLOCENE_TIMESTAMP;
-/// Isthmus base hardfork activation timestamp
+/// Isthmus base hardfork activation timestamp is 1746806401.
 pub const BASE_MAINNET_ISTHMUS_TIMESTAMP: u64 = OP_MAINNET_ISTHMUS_TIMESTAMP;
